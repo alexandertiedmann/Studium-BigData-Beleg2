4ebba9720af345af12e970f191ccf163cc6b4b58
Alexander Tiedmann <s0556127@htw-berlin.de>
2018-06-29 13:49:48 +0200

    complete first 7 functions

--- src/main/scala/loganalyse/LogAnalyseFuns.scala
+++ src/main/scala/loganalyse/LogAnalyseFuns.scala
@@ -13,55 +13,94 @@ object LogAnalyseFuns {
       val failed_logs= parsed_logs.filter(_._2==0).map(_._1)
       (parsed_logs, access_logs, failed_logs)
   }
-  
-  def calculateLogStatistic(data:RDD[Row]):(Long,Long,Long)= ???
-  /* 
+
+  /*
    * Calculate for the content size the following values:
-   * 
+   *
    * minimum: Minimum value
    * maximum: Maximum value
    * average: Average
-   * 
+   *
    * Return the following triple:
    * (min,max,avg)
    */
-  
-  
-  def getResponseCodesAndFrequencies(data:RDD[Row]):List[(Int,Int)]= ???
-  
-  /* 
+  def calculateLogStatistic(data:RDD[Row]):(Long,Long,Long)= {
+    val size = data.map( f => f.getInt(8)) //aus der Row die Content Size
+    (
+      size.min(), //Minimum berechnen
+      size.max(), //Maximum berechnen
+      size.sum().toLong / size.count() //Durschnitt berechnen
+    )
+  }
+
+
+  /*
    * Calculate for each single response code the number of occurences
    * Return a list of tuples which contain the response code as the first
    * element and the number of occurences as the second.
-   *    *   
+   *    *
    */
-    
-  
-  def get20HostsAccessedMoreThan10Times(data:RDD[Row]):List[String]= ???
-  
-  /* 
-   * Calculate 20 abitrary hosts from which the web server was accessed more than 10 times 
+  def getResponseCodesAndFrequencies(data:RDD[Row]):List[(Int,Int)]= {
+    data.groupBy(f => f.getInt(7)) //aus der Row den Response Code gruppieren
+      .map{ //eine map mit den codes und der anzahl der responses
+        case (code, responses) => (code, responses.count(_ => true))
+      }
+      .collect() //gibt die map als dataset zurück
+      .toList //in eine Liste
+  }
+
+  /*
+   * Calculate 20 arbitrary hosts from which the web server was accessed more than 10 times
    * Print out the result on the console (no tests take place)
    */
-  
-  def getTopTenEndpoints(data:RDD[Row]):List[(String, Int)]= ???
-  
-  /* 
-   * Calcuclate the top ten endpoints. 
+  def get20HostsAccessedMoreThan10Times(data:RDD[Row]):List[String]= {
+    data.groupBy(f => f.getString(0)) //alle Hosts aus der Row und gruppieren
+      .map [(String,Int)]{ //eine map mit dem Namen und der Anzahl
+      //wenn host name in anderer Zeile dann counten
+        case (host, it: Iterable[Row])  => (host, it.count(_ => true))
+      }
+      .filter { case (_ , count) => count > 20 } //nach Hosts mit Count 20 filtern
+      .map{ case (host, _) => host } //diese in eine neue map
+      .take(20) //aus der map 20 herausnehmen und in ein Array schreiben
+      .toList //das array in eine Liste umwandeln
+  }
+
+  /*
+   * Calcuclate the top ten endpoints.
    * Return a list of tuples which contain the path of the endpoint as the first element
    * and the number of accesses as the second
    * The list should be ordered by the number of accesses.
    */
-      
-  def getTopTenErrorEndpoints(data:RDD[Row]):List[(String, Int)]= ???
-  
-  /* 
+  def getTopTenEndpoints(data:RDD[Row]):List[(String, Int)]= {
+    data.groupBy(f => f.getString(5)) //alle angefragten Endpoints
+      .map { //eine map mit Name des Endpoints und Anzahl der Anfragen
+        case (endpoint, responses) => (endpoint, responses.count(_ => true))
+      }
+      .sortBy(x=>x._2, ascending = false) //sortieren
+      .take(10) //die ersten 10 herausnehmen
+      .toList //in eine Liste
+  }
+
+  /*
    * Calculate the top ten endpoint that produces error response codes (response code != 200).
-   * 
+   *
    * Return a list of tuples which contain the path of the endpoint as the first element
    * and the number of errors as the second.
    * The list should be ordered by the number of accesses.
    */
+  def getTopTenErrorEndpoints(data:RDD[Row]):List[(String, Int)]= {
+    //alle angefragten ResponseCodes die nicht 200 sind rausfiltern
+    data.filter(f => f.getInt(7) != 200)
+      .groupBy(f => f.getString(5)) // mit den Endpoints gruppieren
+      .map { //eine map mit Name des Endpoints und den Fehlgeschlagenen Anfragen
+        row => (row._1, row._2.count(i => true))
+      }
+      .sortBy(x=>x._2, ascending = false) //sortieren
+      .take(10) //die ersten 10 herausnehmen
+      .toList //in eine Liste
+  }
+  
+
   
   def getNumberOfRequestsPerDay(data:RDD[Row]):List[(Int,Int)]= ???
   
@@ -118,7 +157,7 @@ object LogAnalyseFuns {
    * Order the list by the hour-number.
    */
 
-  
+  //Test noch falsch!!!
   def getAvgRequestsPerWeekDay(data:RDD[Row]):List[(Int,String)]= ???
   
   /*
